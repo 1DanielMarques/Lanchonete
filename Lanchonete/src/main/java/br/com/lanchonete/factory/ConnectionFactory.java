6b98047b2c7fac19c32dee05423b80a4ff1e883d
mDanielSE <daniel.marques@edu.unifil.br>
2022-08-25 14:28:08 -0300

    conexao_com_banco_feita

--- Lanchonete/src/main/java/br/com/lanchonete/factory/ConnectionFactory.java
+++ Lanchonete/src/main/java/br/com/lanchonete/factory/ConnectionFactory.java
@@ -1,4 +1,28 @@
 package br.com.lanchonete.factory;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class ConnectionFactory {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/lanchonete";
+
+    public static Connection createConnection() throws Exception {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+        return connection;
+    }
+    public static void main(String[] args) throws Exception {
+        Connection con = createConnection();
+        if (con != null) {
+            System.out.println("Conectou!");
+            con.close();
+        }
+
+    }
+
 }
