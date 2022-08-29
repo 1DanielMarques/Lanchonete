45b2d77cbcbc30153ba763a8bd6af48ba57769d7
mDanielSE <daniel.marques@edu.unifil.br>
2022-08-29 17:01:35 -0300

    CRUD bebidas completo

--- Lanchonete/src/main/java/br/com/lanchonete/aplicacao/Main.java
+++ Lanchonete/src/main/java/br/com/lanchonete/aplicacao/Main.java
@@ -13,8 +13,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Bebida bebida = new Bebida();
-        BebidaDAO bebidaDao = new BebidaDAO();
         int opcao = 0;
         Scanner sc = new Scanner(System.in);
 
@@ -111,13 +109,97 @@ public class Main {
                                 break;
                         }
                     } while (opcao != 9);
+                    opcao = 0;
                     break;
                 case 2:
-                    System.out.println("bebidas");
+                    String marca, auxB, respostaB = "", litragem = "";
+                    double preco_custo, preco_venda;
+                    int idB = 0;
+                    Bebida bebidaId = new Bebida();
+                    Bebida bebida = new Bebida();
+                    BebidaDAO bebidaDao = new BebidaDAO();
+
+                    do {
+                        opcao = 0;
+
+                        System.out.println("1 - Registrar Bebida");
+                        System.out.println("2 - Mostrar Bebidas");
+                        System.out.println("3 - Alterar Bebida");
+                        System.out.println("4 - Deletar Bebida");
+                        System.out.println("9 - Finalizar");
+                        opcao = sc.nextInt();
+                        switch (opcao) {
+                            case 1:
+                                System.out.println("----REGISTRAR----");
+                                System.out.print("[MARCA]       | ");
+                                auxB = sc.nextLine();
+                                marca = sc.nextLine();
+                                System.out.print("[LITRAGEM]    | ");
+                                litragem = sc.next();
+                                System.out.print("[VALOR CUSTO] | ");
+                                preco_custo = sc.nextDouble();
+                                System.out.print("[VALOR VENDA] | ");
+                                preco_venda = sc.nextDouble();
+
+                                bebida.setMarca(marca);
+                                bebida.setLitro(litragem);
+                                bebida.setPreco_custo(preco_custo);
+                                bebida.setPreco_venda(preco_venda);
+                                bebidaDao.create(bebida);
+                                break;
+                            case 2:
+                                System.out.println("--------BEBIDAS--------");
+                                for (Bebida b : bebidaDao.read()) {
+                                    b.dados(b);
+                                    System.out.println("-----------------------");
+                                }
+                                break;
+                            case 3:
+                                System.out.println("-----ATUALIZAR-----");
+                                System.out.print("[ID]          | ");
+                                idB = sc.nextInt();
+                                System.out.print("[MARCA]       | ");
+                                auxB = sc.nextLine();
+                                marca = sc.nextLine();
+                                System.out.print("[LITRAGEM]    | ");
+                                litragem = sc.next();
+                                System.out.print("[VALOR CUSTO] | ");
+                                preco_custo = sc.nextDouble();
+                                System.out.print("[VALOR VENDA] | ");
+                                preco_venda = sc.nextDouble();
+
+                                bebida.setId(idB);
+                                bebida.setMarca(marca);
+                                bebida.setLitro(litragem);
+                                bebida.setPreco_custo(preco_custo);
+                                bebida.setPreco_venda(preco_venda);
+                                bebidaDao.update(bebida);
+                                break;
+                            case 4:
+                                System.out.println("-----DELETAR-----");
+                                System.out.print("Informe o ID: ");
+                                idB = sc.nextInt();
+                                for (Bebida bId : bebidaDao.read()) {
+                                    if (bId.getId() == idB) {
+                                        bebidaId = bId;
+                                        break;
+                                    }
+                                }
+                                System.out.println("-------BEBIDA-------");
+                                bebidaId.dados(bebidaId);
+                                System.out.println("--------------------");
+                                System.out.println("Deseja excluir essa BEBIDA? S/N");
+                                respostaB = sc.next();
+                                respostaB.toLowerCase();
+                                if (respostaB.equals("s")) {
+                                    bebidaDao.delete(idB);
+                                }
+                                break;
+                        }
+                    } while (opcao != 9);
+                    opcao = 0;
                     break;
             }
-
-
         } while (opcao != 9);
     }
 }
